10d47a4d56bd4d1b02fdb90ba69f752a8d167a0e
SemRodenburg <sem.rodenburg@icloud.com>
2024-02-07 14:46:00 +0100

    Added project to Github

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+    return(
+        <div className="nav">
+            <img className="nav--logo" src="../images/meme-icon.png" alt="Meme Icon"/>
+            <h1 className="nav--title">Meme Generator</h1>
+            <h2 className="nav--text">React course - Project 3</h2>
+        </div>
+    )
+}
