e586a9446b3b1f5443d3adcbf59e073bb26cf0bc
SemRodenburg <74918158+SemRodenburg@users.noreply.github.com>
2024-02-07 16:53:42 +0100

    Added update Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,6 @@ export default function Navbar() {
         <div className="nav">
             <img className="nav--logo" src="../images/meme-icon.png" alt="Meme Icon"/>
             <h1 className="nav--title">Meme Generator</h1>
-            <h2 className="nav--text">React course - Project 3</h2>
         </div>
     )
 }
